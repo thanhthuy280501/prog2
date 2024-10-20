a4118fb4682835db185a59d228b4cd39bb8482d0
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-10-19 22:20:29 -0400

    book.cpp

---
+++ Book.cpp
@@ -0,0 +1,42 @@
+#include "Book.h"
+#include <algorithm>
+#include <cctype>
+
+// Constructors
+Book::Book() : title(""), authorFirstName(""), authorLastName(""), ISBN(""), publisher("") {}
+
+Book::Book(const std::string& title, const std::string& authorFirst, const std::string& authorLast, const std::string& ISBN, const std::string& publisher)
+    : title(title), authorFirstName(authorFirst), authorLastName(authorLast), ISBN(ISBN), publisher(publisher) {}
+
+// Getter methods
+std::string Book::getTitle() const { return title; }
+std::string Book::getAuthorFirstName() const { return authorFirstName; }
+std::string Book::getAuthorLastName() const { return authorLastName; }
+std::string Book::getISBN() const { return ISBN; }
+std::string Book::getPublisher() const { return publisher; }
+
+// Setter methods
+void Book::setTitle(const std::string& title) { this->title = title; }
+void Book::setAuthorFirstName(const std::string& authorFirstName) { this->authorFirstName = authorFirstName; }
+void Book::setAuthorLastName(const std::string& authorLastName) { this->authorLastName = authorLastName; }
+void Book::setISBN(const std::string& ISBN) { this->ISBN = ISBN; }
+void Book::setPublisher(const std::string& publisher) { this->publisher = publisher; }
+
+// String manipulation methods
+void Book::titleToUpper() {
+    std::transform(title.begin(), title.end(), title.begin(), ::toupper);
+}
+
+void Book::titleToLower() {
+    std::transform(title.begin(), title.end(), title.begin(), ::tolower);
+}
+
+void Book::authorToUpper() {
+    std::transform(authorFirstName.begin(), authorFirstName.end(), authorFirstName.begin(), ::toupper);
+    std::transform(authorLastName.begin(), authorLastName.end(), authorLastName.begin(), ::toupper);
+}
+
+void Book::authorToLower() {
+    std::transform(authorFirstName.begin(), authorFirstName.end(), authorFirstName.begin(), ::tolower);
+    std::transform(authorLastName.begin(), authorLastName.end(), authorLastName.begin(), ::tolower);
+}
